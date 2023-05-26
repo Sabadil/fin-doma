a3fd2e796345cf09838502c9198cf36ed4427c42
Ivan Zautorin <zautorin@gmail.com>
2023-05-26 17:28:45 +0300

    about

--- _src/app.js
+++ _src/app.js
@@ -5,7 +5,18 @@ Alpine.start();
 import './fonts.css';
 import './reset.css';
 import './app.scss';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
 document.addEventListener('DOMContentLoaded', () => {
-
+    const swiperOurProjects = new Swiper(".our-porject-swiper", {
+        centeredSlides: true,
+        autoplay: {
+        delay: 4000,
+        loop: true,
+        disableOnInteraction: false,
+      },
+        parallax: true,
+        speed: 1200,
+      });
 })
